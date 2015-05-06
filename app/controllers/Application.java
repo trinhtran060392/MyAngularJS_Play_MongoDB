8bcdba163083715d094c1c2037e9ec2865bb59bf
TrinhTV3 <trinhtv060392@gmail.com>
2015-05-06 14:08:13 +0700

    add dependency to use serves from another project

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,4 +1,44 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Result;
 
+public class Application extends Controller {
+
+  public Result allStudents() {
+    return ok();
+  }
+  
+  public Result createStudent() {
+    return ok();
+  }
+  
+  public Result getStudent(String id) {
+    return ok();
+  }
+  
+  public Result doUpdateStudent(String id) {
+    return ok();
+  }
+  
+  public Result deleteStudent(String id) {
+    
+    return ok();
+  }
+  
+  public Result checkLogin() {
+    return ok();
+  }
+  
+  public Result getBoyStudent() {
+    return ok();
+  }
+  
+  public Result getGirlStudent() {
+    return ok();
+  }
+  
+  public Result getStudentsByPage() {
+    return ok();
+  }
+}
--- build.sbt
+++ build.sbt
@@ -8,7 +8,8 @@ libraryDependencies ++= Seq(
   cache,
   "org.mongodb" % "mongo-java-driver" % "2.12.4",
   "com.google.inject" % "guice" % "4.0-beta",
-  "com.google.inject.extensions" % "guice-assistedinject" % "3.0"
+  "com.google.inject.extensions" % "guice-assistedinject" % "3.0",
+  "com.trinhtv3.fsoft" % "DesignBackEndLearning" % "1.0-SNAPSHOT"
 )     
 
 play.Project.playJavaSettings
